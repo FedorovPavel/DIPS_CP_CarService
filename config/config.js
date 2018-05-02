b576435c2fda75c975a297a7a7756ce2f6171402
Pavel <IU7.2@yandex.ru>
2018-05-02 23:54:20 +0300

    изменена БД для тестирования

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ const config = {
       name: 'car-service'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/car-service-test'
+    db: 'mongodb://Tester:1111@ds111370.mlab.com:11370/mongodips'
   },
 
   production: {
